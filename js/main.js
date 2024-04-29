e6306c5c4a4a09b67d074a89e5e21c96768d1616
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-04-29 18:25:46 +0530

    implement jquery  plugin step

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+jQuery(document).ready(function(){
+  $(".step-content-wrapper").steps({
+    headerTag: "h3",
+    bodyTag: ".radio-checkbox-listing",
+    transitionEffect: "slideLeft",
+    autoFocus: true
+});
+});
