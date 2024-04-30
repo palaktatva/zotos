0fcf07577de904bc22b7ebf11fa135429cab6985
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-04-30 18:27:03 +0530

    apply changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 
-
 jQuery(document).ready(function(){
-  $(".step-content-wrapper").steps({
-    headerTag: "h3",
-    bodyTag: ".radio-checkbox-listing",
-    transitionEffect: "slideLeft",
-    autoFocus: true
-});
+
+jQuery(".step-content-wrapper").steps({
+  headerTag: "span",
+  bodyTag: ".steps-content-block", 
+  transitionEffect: "slideLeft",
+  cssClass: "tabcontrol",
+
+  labels: {
+    previous: "Back"
+  }
+  });
+
+  jQuery(".actions a[href='#next'],a[href='#finish']").click(function(){
+    console.log(jQuery(this).closest(".quiz-section").find(".progress-bar-inner").children("span").animate({width: "+=10%"}));
+  });
+
+  // jQuery(".quiz-section").find(".step-content-wrapper").each(function(){
+  //   var step_block_height = jQuery(".steps-content-block").innerHeight();
+  //   console.log(step_block_height);
+  // })
+  
 });
